352731a81f3ce8f6589edece2db5157ceaac8561
David Ladowitz <david@ladowitz.com>
2014-03-01 16:00:38 -0800

    Initial Commit

---
+++ Counters/IOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IOSAppDelegate.h
+//  Counters
+//
+//  Created by David Ladowitz on 3/1/14.
+//  Copyright (c) 2014 Little Cat Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IOSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Counters/IOSViewController.h
@@ -0,0 +1,13 @@
+//
+//  IOSViewController.h
+//  Counters
+//
+//  Created by David Ladowitz on 3/1/14.
+//  Copyright (c) 2014 Little Cat Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IOSViewController : UIViewController
+
+@end
